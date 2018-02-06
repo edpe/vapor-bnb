58dc4904ea292bc1c67fbfd1db6ce39e8a5addbb
Ellie <elliewem@gmail.com>
2018-02-06 13:07:01 +0000

    Encrypt passwords

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,20 @@
+require 'bcrypt'
+
 class User
   include DataMapper::Resource
+  include BCrypt
 
-  property :id,       Serial
-  property :email,    String
-  property :password, Text
-  property :name,     String
-  property :username, String
+  attr_reader :password
+
+  property :id,                Serial
+  property :email,             String
+  property :password_hash,     Text
+  property :name,              String
+  property :username,          String
+
+  def password=(password)
+    @password = password
+    self.password_hash = BCrypt::Password.create(password)
+  end
 
 end
